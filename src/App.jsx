7e0d3b7d0f5d07e3335947cd78aa98e70429efe3
Martin <martin_acevski@outlook.com>
2024-03-06 14:17:30 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+	return (
+		<div className="container">
+			<Header />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,96 @@
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import classes from "./Footer.module.css";
+import PropTypes from "prop-types";
+
+const MenuItem = ({ icon, text }) => (
+	<div className={classes.myProfile}>
+		<img
+			className={`${classes.profilep} ${
+				text === "Martin Acevski" ? classes.specialProfile : ""
+			}`}
+			src={icon}
+			alt={text}
+		/>
+		<p className={classes.name}>{text}</p>
+	</div>
+);
+
+MenuItem.propTypes = {
+	icon: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+};
+
+const Footer = () => {
+	const menuItems = [
+		{ icon: "../../../public/maci.jpg", text: "Martin Acevski" },
+		{ icon: "../../../public/friends.png", text: "Friends" },
+		{ icon: "../../../public/memories.png", text: "Memories" },
+		{ icon: "../../../public/saved.png", text: "Saved" },
+		{ icon: "../../../public/groupFuter.png", text: "Group" },
+		{ icon: "../../../public/videoFooter.png", text: "Video" },
+	];
+
+	const shortcuts = [
+		{
+			icon: "../../../public/itsektor.jpg",
+			text: "Сојуз Комитет на Програмери на Република Македонија",
+		},
+		{ icon: "../../../public/avtoapaza.jpg", text: "AVTOPAZAR MAKEDONIJA" },
+		{
+			icon: "../../../public/istoProgramiranje.jpg",
+			text: "Работа и Вработување во ИТ Сектор",
+		},
+		{
+			icon: "../../../public/lastjob.jpg",
+			text: "Сојуз Комитет на Програмери на Република Македонија",
+		},
+	];
+
+	return (
+		<div className={classes.container}>
+			<div className={classes.content}>
+				{menuItems.map((item, index) => (
+					<MenuItem key={index} icon={item.icon} text={item.text} />
+				))}
+				<button className={classes.more}>
+					<KeyboardArrowDownIcon
+						sx={{
+							color: "white",
+							backgroundColor: "#3a3b3c;",
+							borderRadius: "20px",
+							width: "30px",
+							height: "30px",
+						}}
+					/>
+					<p className={classes.name}>See more</p>
+				</button>
+				<div className={classes.border}></div>
+
+				<div className={classes.shortCuts}>
+					<p className={classes.title}>Your shortcuts</p>
+					{shortcuts.map((shortcut, index) => (
+						<MenuItem
+							key={index}
+							icon={shortcut.icon}
+							text={shortcut.text}
+						/>
+					))}
+					<button className={classes.more}>
+						<KeyboardArrowDownIcon
+							sx={{
+								color: "white",
+								backgroundColor: "#3a3b3c;",
+								borderRadius: "20px",
+								width: "30px",
+								height: "30px",
+							}}
+						/>
+						<p className={classes.name}>See more</p>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
