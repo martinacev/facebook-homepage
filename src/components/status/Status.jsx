7a84ff94c15a23f1e8e829a449b91f6b8256cd9c
Martin <martin_acevski@outlook.com>
2024-03-07 15:54:21 +0100

    adding-likes-com-share

--- src/components/status/Status.jsx
+++ src/components/status/Status.jsx
@@ -1,6 +1,10 @@
 import classes from "./Status.module.css";
 import mario from "/public/mark.jpg";
 import image from "/public/programin-learn.jpg";
+import share from "/public/share.png";
+import comment from '/public/comment.png';
+import like from '/public/like.png'
+
 
 const Status = () => {
 	return (
@@ -25,9 +29,7 @@ const Status = () => {
 								height="20"
 								fill="currentColor"
 							>
-								<g
-									transform="translate(-446 -350)"
-								>
+								<g transform="translate(-446 -350)">
 									<path d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0"></path>
 								</g>
 							</svg>
@@ -46,6 +48,34 @@ const Status = () => {
 						alt="learn-programing-post"
 					/>
 				</div>
+				
+				<div className={classes.components}>
+					<div className={classes.holder}>
+						<img
+							className={classes.emoji}
+							src={like}
+							alt="video call"
+						/>
+						<p className={classes.description}>Like</p>
+					</div>
+					<div className={classes.holder}>
+						<img
+							className={classes.emoji}
+							src={comment}
+							alt="gallery"
+						/>
+						<p className={classes.description}>Comment</p>
+					</div>
+					<div className={classes.holder}>
+						<img
+							className={classes.emoji}
+							src={share}
+							alt="activity"
+						/>
+						<p className={classes.description}>Send</p>
+					</div>
+				</div>
+				<div className={classes.border}></div>
 			</div>
 		</div>
 	);
