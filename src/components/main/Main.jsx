1cb8bc12b0336ee814f3ac7200cdb5e49e6650dd
Martin <martin_acevski@outlook.com>
2024-03-07 13:46:41 +0100

    adding-image

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,13 +2,14 @@ import classes from "./Main.module.css";
 
 import Story from "../story/Story";
 import Post from "../post/Post";
+import Status from "../status/Status";
 
 const Main = () => {
 	return (
 		<div className={classes.container}>
 			<Story />
-             <Post />
-		
+			<Post />
+			<Status />
 		</div>
 	);
 };
---
+++ src/components/status/Status.jsx
@@ -0,0 +1,54 @@
+import classes from "./Status.module.css";
+import mario from "/public/mark.jpg";
+import image from "/public/programin-learn.jpg";
+
+const Status = () => {
+	return (
+		<div className={classes.container}>
+			<div className={classes.status}>
+				<div className={classes.wrapper}>
+					<img
+						className={classes.profilePic}
+						src={mario}
+						alt="profile"
+					/>
+					<div className={classes.information}>
+						<p className={classes.name}>Mario Bozidarovski</p>
+						<p className={classes.time}>38m</p>
+					</div>
+
+					<div className={classes.dots}>
+						<div className={classes.background}>
+							<svg
+								viewBox="0 0 20 20"
+								width="20"
+								height="20"
+								fill="currentColor"
+							>
+								<g
+									transform="translate(-446 -350)"
+								>
+									<path d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0"></path>
+								</g>
+							</svg>
+						</div>
+
+						<div className={classes.cross}>
+							<p className={classes.simbol}>✕</p>
+						</div>
+					</div>
+				</div>
+
+				<div className={classes.image}>
+					<img
+						className={classes.photo}
+						src={image}
+						alt="learn-programing-post"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Status;
